67f00f0f9f3a1fa50424211da2cfd59e9255d08b
Hommyah <evil.hom26@gmail.com>
2024-12-06 00:16:53 +0300

    hw1

--- main.py
+++ main.py
@@ -1,2 +1,10 @@
-if __name__ == '__main__':
-    print(1)
+#if __name__ == '__main__':
+    #1
+list = [1,2,3,4,5]
+print('N1: ', list[::-1])
+    #2
+a = "A string with white space"
+print('N2: ', a.replace(' ',''))
+    #3
+d = {'id':7, 'name':'Shiba', 'color':'brown', 'speed':'very slow'}
+print('N3: ', d.keys())
